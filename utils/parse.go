51ea3f1853609324e5caa1dc8b21369e9ce0845e
Blessium <alessandro.melgar04@gmail.com>
2023-05-26 18:07:00 +0200

    minor fix

--- utils/parse.go
+++ utils/parse.go
@@ -6,17 +6,17 @@ import (
 )
 
 var (
-    IdParseError="id is not an unsigned integer"
-    IdNegativeError="id cannot be negative"
+    ErrUnsignedInt="id is not an unsigned integer"
+    ErrNegativeId="id cannot be less than 0"
 )
 
 func StringToUint(id string) (uint, error) {
     i, err := strconv.Atoi(id)
     if err != nil {
-        return 0, errors.New(IdParseError)
+        return 0, errors.New(ErrUnsignedInt)
     }
     if i < 0 {
-        return 0, errors.New(IdNegativeError)
+        return 0, errors.New(ErrNegativeId)
     }
     return uint(i), nil
 }
